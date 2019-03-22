5d12cda6df53961851d4ec1cc72a676c94ea3c93
Ramon Chavez <rachavez1974@gmail.com>
2019-03-22 01:58:08 -0400

    Finish Badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)  
+  badge_messages = []
+    names.each do |name|
+      badge_messages.push(badge_maker(name))
+    end
+  badge_messages
+end
+
+def assign_rooms(speakers)
+  assigned_rooms = []
+    speakers.each_with_index do |name, index|
+      #assigned_rooms[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+      assigned_rooms.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+    end
+  assigned_rooms
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+    badges.each do |badge|
+      puts badge
+    end
+  speakers = []
+  rooms = assign_rooms(attendees)
+    rooms.each do |room|
+      puts room
+    end  
+end
+
